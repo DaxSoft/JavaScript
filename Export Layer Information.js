39436fb7d2022bd4af3824751880de8d698acb2f
Dax Soft <dax-soft@live.com>
2017-11-26 14:40:29 -0200

    Export Layer Information 1.0

--- Export Layer Information.js
+++ Export Layer Information.js
@@ -1,104 +1,141 @@
 /*
-Export Layer Information
-Author: Dax Soft
-Version: 0.1
-Website: www.dax-soft.weebly.com
-Just WORKS on WINDOW!
+===============================================================================
+[Export Layer Information]
+by Dax Soft | Kvothe
+www.dax-soft.weebly.com
+1.0
 ===============================================================================
 Installation Guide:
   1. Place this script in /filename: Export Layer Information.js
-    Mac: '~/Applications/Adobe Photoshop CS#/Presets/Scripts/'
     Win: 'C:\Program Files\Adobe\Adobe Photoshop CS#\Presets\Scripts\'
   2. Restart your Photoshop
-  3. Choose File > Scripts >  Export Layer Information
-  4. Or just go to Choose File > Scripts > Search (Sort by .js files)
+  3. Choose File > Scripts > Export Layer Information
+  4. Or Just go to Choose File > Scripts > Search (Sort by .js files)
 ===============================================================================
 */
-'use strict';
-// Imported Session
+// [Imported]
 var Imported = Imported || {};
-var Dax = Dax || {};
-Dax.exportLayerInformation = {};
-// That Function
+Imported.Haya = Imported.Haya || {};
+Imported.Haya.exportLayerInformation = {};
+
+// [Script]
 (function($) {
-  // Enables lauchin'
-  #target photoshop
-  // Bring application forward
-  app.bringToFront();
-  // Set active Document variable and decode name for output
-  var docRef = app.activeDocument;
-  var docName = decodeURI(activeDocument.name);
-  // Define PIXELS as measurement
-  var defaultRulerUnits = preferences.rulerUnits;
-  preferences.rulerUnits = Units.PIXELS;
-  // Number of layers
-  var layerNum = app.activeDocument.artLayers.length;
-  // Active Layers
-  var layer = app.activeDocument.activeLayer;
-  // Getting the position
-  var arrayInfo = new Array();
-  arrayInfo["x"] = layer.bounds[0].value;
-  arrayInfo["y"] = layer.bounds[1].value;
-  arrayInfo["Opacity"] = layer.opacity;
-  var stringInformation = "";
-  // All layer each
-  //   current : current layer
-  function eachLayer(current) {
-    for ( var i = 0; i < current.layers.length; i++ ) { // each layer display on this
-      layer = current.layers[i];
-      // Getting Information
-      arrayInfo["x"] = layer.bounds[0].value;
-      arrayInfo["y"] = layer.bounds[1].value;
-      arrayInfo["Opacity"] = layer.opacity;
-      stringInformation += layer.name + ", position: " + arrayInfo["x"] + "x; " + arrayInfo["y"] + "y;\n";
-      stringInformation += layer.name + ", opacity: " + arrayInfo["Opacity"] + "\n\n";
-      // testing
-      if (isLayerSetup(current.layers[i])) { // testing if is set
-        eachLayer(currLayers.layers[i]); // run this loop
-      }
+'use strict';
+    // [setup]
+    #target photoshop
+    app.bringToFront();
+    // document variable
+    var docRef = app.activeDocument;
+    var docName = decodeURI(activeDocument.name);
+    var currentPath =  (app.activeDocument.fullName.parent.fsName).toString().replace(/\\/g, '/') + "/";
+    var string = "{\n\n";
+    var filename = "";
+    // create a new folder
+    var createFolder = new Folder(currentPath + String(docName.split(".")[0]));
+    if (!createFolder.exist) createFolder.create();
+    // root
+    var root = currentPath + String(docName.split(".")[0]) + "/";
+    // add json info
+    function addJson(hash) {
+        this.string = "";
+        hash.space = hash.space === undefined ? "\t\t" : hash.space;
+        hash.break = hash.break === undefined ? "\n" : hash.break;
+        this.string += hash.space + '"' + hash.key + '": ' + hash.value + "," + hash.break;
+        return this.string;
     }
-  }
-
-  // test for a layer set
-  function isLayerSetup(current) {
-    try {
-      if (current.layer.length > 0) {
-        return true;
-      }
+    // default rules
+    $.rules = preferences.rulerUnits;
+    preferences.rulerUnits = Units.PIXELS;
+    // get layer information
+    $.layer = {
+        // number
+        number: app.activeDocument.artLayers.length,
+        // layer
+        data: app.activeDocument.activeLayer,
     }
-    catch(error) {
-      return false
+    // information data
+    $.data = {
+        name:               new Array(),
+        x:                  new Array(),
+        y:                  new Array(),
+        blendMode:          new Array(),
+        isBackgroundLayer:  new Array(),
+        kind:               new Array(),
+        positionLocked:     new Array(),
+        filename:           new Array(),
+        opacity:            new Array() 
     }
-  }
-
-  // Asking for a Folder to Save it
-  var FPath = Folder.selectDialog("Save exported information to");
-  fileLineFeed = "Windows";
-
-  // Exporting all information
-  function exportInformation(info) {
-    try {
-      var file = new File(FPath + "/" + docName + ".txt");
-      file.remove();
-      file.open("a");
-      file.lineFeed = fileLineFeed;
-      file.write(info);
-      file.close();
+    // bring out all visible layer
+    function bringOutEachLayerVisible() {
+        var visibleLayers = [];
+        visibleLayers = collectAllLayersVisible(docRef, visibleLayers);
+        for (var i = 0; i < visibleLayers.length; i++) {
+            var current = visibleLayers[i];
+            // get out the information
+            $.data.name.push(current.name);
+            $.data.x.push(current.bounds[0].value);
+            $.data.y.push(current.bounds[1].value);
+            $.data.blendMode.push(current.blendMode);
+            $.data.opacity.push(current.opacity);
+            $.data.kind.push(current.kind);
+            $.data.isBackgroundLayer.push(current.isBackgroundLayer);
+            $.data.positionLocked.push(current.positionLocked);
+            if (current.name == ":") {
+                $.data.filename.push(current.name.split(":")[0])
+            } else {
+                $.data.filename.push(current.name);
+            }
+        }
     }
-    // don't
-    catch(error) {}
-  }
-
-  // Running the script
-  eachLayer(docRef);
-  preferences.rulerUnits = defaultRulerUnits; // Set preferences back to user 's defaults
-  exportInformation(stringInformation);
-
-  // Results to feedback
-  if (FPath == null) {
-    alert("Exported aborted", "Cancel")
-  } else {
-    alert("Exported as txt file: " + layerNum + "layer's info to " + FPath + "/" + docName + "~ using (" + fileLineFeed + ") line feeds!", "Congratulations!");
-  }
-})(Dax.exportLayerInformation);
-Imported["exportLayerInformation"] = true;
+    // collect all Layers
+    function collectAllLayersVisible(parent, allLayers) {
+        for (var m = 0; m < parent.layers.length; m++) {
+            var currentLayer = parent.layers[m];
+            if (currentLayer.typename === "ArtLayer") {
+                if (currentLayer.visible) {
+                    allLayers.push(currentLayer);
+                }
+            } else {
+                // recursive
+                collectAllLayersVisible(currentLayer, allLayers);
+            }
+        }
+        return allLayers;
+    }
+    // test out for a layer set
+    function isLayerSetup(current) { try { if (current.layer.length > 0) return true; } catch(error) { return false; } }
+    // [FOLDER]
+    fileLineFeed = "Windows";
+    // Export information
+    function exportInformation() {
+        // [string]
+        for (var index = 0; index < $.data.name.length; index++) {
+            string += "\t" + '"' + $.data.name[index] + '": {\n';
+            string += addJson({key: "filename", value: '"' + $.data.filename[index] + '"'});
+            string += addJson({key: "blendMode", value: '"' + $.data.blendMode[index] + '"'});
+            string += addJson({key: "x", value: $.data.x[index]});
+            string += addJson({key: "y", value: $.data.y[index]});
+            string += addJson({key: "kind", value: '"' + $.data.kind[index] + '"'});
+            string += addJson({key: "isBackgroundLayer", value: $.data.isBackgroundLayer[index]});
+            string += addJson({key: "positionLocked", value: $.data.positionLocked[index]});
+            string += "\t\t" + '"opacity": ' + String($.data.opacity[index]) + "\n";
+            string += index === $.data.name.length-1 ? "\t}\n" :  "\t},\n\n";
+        }
+        // out
+        string += "}";
+        // [file]
+        var file = new File(root + "file.json");
+        file.remove();
+        file.open("a");
+        file.lineFeed = fileLineFeed;
+        file.write(string);
+        file.close();
+        // [exported]
+        alert("Exported at: " + root, "Finished!");
+    }
+    // [RUNNING OUT]
+    bringOutEachLayerVisible();
+    preferences.rulerUnits = $.rules; // Set preferences back to user 's defaults
+    exportInformation();
+})(Imported.Haya.exportLayerInformation);
+Imported['exportLayerInformation'] = true;
